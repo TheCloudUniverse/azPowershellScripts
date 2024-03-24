cd6170876532bdc2537556834a2954970054279a
TheCloudUniverse <140804201+TheCloudUniverse@users.noreply.github.com>
2024-03-24 12:47:12 +0530

    Update FindCustomSQLFirewallRules.ps1

--- FindCustomSQLFirewallRules.ps1
+++ FindCustomSQLFirewallRules.ps1
@@ -22,20 +22,20 @@ function FindIPType {
 }
 
 Get-AzSqlServerFirewallRule -ResourceGroupName $ResourceGroup -ServerName $SqlServer > FirewallRules.txt
-Get-Content -Path 'C:\Users\navee\Documents\TheCloudUniverse\PSscripts\FirewallRules.txt' |
+Get-Content -Path 'C:\TheCloudUniverse\PSscripts\FirewallRules.txt' |
 	Select-String -Pattern '\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b' -AllMatches |
 	ForEach-Object { $_.Matches.Value } > IpsList.txt
 
-$filePath = "C:\Users\navee\Documents\TheCloudUniverse\PSscripts\IpsList.txt"
+$filePath = "C:\TheCloudUniverse\PSscripts\IpsList.txt"
 $unique = Get-Content $filePath | Sort-Object | Get-Unique
 Set-Content -Path $filePath -Value $unique
 
 #Get the contnets of the filePath
-$content = Get-Content -Path 'C:\Users\navee\Documents\TheCloudUniverse\PSscripts\IpsList.txt'
+$content = Get-Content -Path 'C:\TheCloudUniverse\PSscripts\IpsList.txt'
 
 #loop
 foreach ($line in $content) {
 	FindIPType $line
 }
-Remove-Item C:\Users\navee\Documents\TheCloudUniverse\PSscripts\FirewallRules.txt
-Remove-Item C:\Users\navee\Documents\TheCloudUniverse\PSscripts\IpsList.txt
+Remove-Item C:\TheCloudUniverse\PSscripts\FirewallRules.txt
+Remove-Item C:\TheCloudUniverse\PSscripts\IpsList.txt
